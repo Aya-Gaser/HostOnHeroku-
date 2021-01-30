ff4fab0fee99776a891923b81ab53c5d2b41a1ab
ayaGaser <ayagaser30@gmail.com>
2021-01-30 21:35:35 +0200

    login update

--- app/Http/Controllers/seedUser.php
+++ app/Http/Controllers/seedUser.php
@@ -4,21 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
-
+use App\Role;
+use App\userRole;
 class seedUser extends Controller
 {
     public function index(){
 
-         DB::table('roles')->insert([
-            'name' => 'admin',
-            'slug' => 'admin'
-            
-        ]);
-        DB::table('users_roles')->insert([
-            'user_id' => 1,
-            'role_id' => 1
-            
-        ]);
+        $role = new Role();
+        $role->name = 'admin';
+        $role->slug = 'admin';
+        $role->save();
+        $roleuser = new userRole();
+        $roleuser->user_id= 1;
+        $roleuser->role_id = 1;
+        $roleuser->save();
+        
       //   return $users;
     }
 }
