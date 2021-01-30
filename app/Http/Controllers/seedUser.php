cba0dea251ebbd70bc747873671a1ac9b3f8b7f8
ayaGaser <ayagaser30@gmail.com>
2021-01-30 23:57:27 +0200

    path update

--- app/Http/Controllers/seedUser.php
+++ app/Http/Controllers/seedUser.php
@@ -4,13 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
-use App\Role;
-use App\userRole;
 class seedUser extends Controller
 {
     public function index(){
-
-        $role = new Role();
+        $user = new User();
+        $user->name = 'Aya';
+        $user->email = 'ayagaser30@gmail.com';
+        $user->userName = 'ayaaaaa';
+         $user->account_type = 'admin';
+         $user->password = bcrypt('123456789');
+         $user->visible = encrypt('123456789');
+         $user->birthdate = ('2021-01-03');
+         $user->save();
+         $users = User::all();
+         /*
+         $role = new Role();
         $role->name = 'admin';
         $role->slug = 'admin';
         $role->save();
@@ -18,7 +26,7 @@ class seedUser extends Controller
         $roleuser->user_id= 1;
         $roleuser->role_id = 1;
         $roleuser->save();
-        
-      //   return $users;
+        */
+         return $users;
     }
 }
