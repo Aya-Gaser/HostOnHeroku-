44ec3c5fe057a7f6d2f55e434afdb84a88f7cbdf
ayaGaser <ayagaser30@gmail.com>
2021-01-31 04:44:06 +0200

    path update

--- app/Http/Controllers/seedUser.php
+++ app/Http/Controllers/seedUser.php
@@ -9,7 +9,7 @@ class seedUser extends Controller
 {
     public function index(){
        
-       /* $user = new User();
+        $user = new User();
         $user->name = 'Aya';
         $user->email = 'ayagaser30@gmail.com';
         $user->userName = 'ayaaaaa';
@@ -19,17 +19,22 @@ class seedUser extends Controller
          $user->birthdate = ('2021-01-03');
          $user->save();
          $users = User::all();
-         */
-        /*$role = new Role();
+         
+        $role = new Role();
+        $role->name = 'admin';
+        $role->slug = 'admin';
+        $role->save();
+        $role = new Role();
         $role->name = 'vendor';
         $role->slug = 'vendor';
         $role->save();
-        /*$roleuser = new userRole();
-        $roleuser->user_id= ;
+        
+        $roleuser = new userRole();
+        $roleuser->user_id= 1;
         $roleuser->role_id = 1;
         $roleuser->save();
          
-         return $users;*/
+         
          $roles = Role::all();
          return $roles;
     }
