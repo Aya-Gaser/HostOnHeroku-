3aea9ec91b6ae23b3af7b0598844dc6632d847a5
ayaGaser <ayagaser30@gmail.com>
2021-07-01 20:01:28 +0200

    roll file input

--- resources/views/layouts/partials/_file_input_plugin_script.blade.php
+++ resources/views/layouts/partials/_file_input_plugin_script.blade.php
@@ -10,31 +10,18 @@
         });
     </script>--}}
 
+{{--
+<script src="{{asset('assets/revolutionary-fileuploader/jquery.fileuploader.min.js')}}"
+        type="text/javascript"></script>
+<script src="{{asset('assets/revolutionary-fileuploader/custom.js')}}" type="text/javascript"></script>
+--}}
 
-
-<!-- the jQuery Library -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
-
-<!-- piexif.min.js is needed for auto orienting image files OR when restoring exif data in resized images and when you
-    wish to resize images before upload. This must be loaded before fileinput.min.js -->
-<script src="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-fileinput@5.2.2/js/plugins/piexif.min.js" type="text/javascript"></script>
-
-<!-- sortable.min.js is only needed if you wish to sort / rearrange files in initial preview. 
-    This must be loaded before fileinput.min.js -->
-<script src="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-fileinput@5.2.2/js/plugins/sortable.min.js" type="text/javascript"></script>
-
-<!-- bootstrap.bundle.min.js below is needed if you wish to zoom and preview file content in a detail modal
-    dialog. bootstrap 5.x or 4.x is supported. You can also use the bootstrap js 3.3.x versions. -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-
-<!-- the main fileinput plugin script JS file -->
-<script src="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-fileinput@5.2.2/js/fileinput.min.js"></script>
-
-<!-- following theme script is needed to use the Font Awesome 5.x theme (`fas`). Uncomment if needed. -->
-<!-- script src="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-fileinput@5.2.2/themes/fas/theme.min.js"></script -->
-
-<!-- optionally if you need translation for your language then include the locale file as mentioned below (replace LANG.js with your language locale) -->
-<script src="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-fileinput@5.2.2/js/locales/LANG.js"></script>
+<script src="{{asset('/assets/bootstrap-fileinput/js/plugins/piexif.js')}}" type="text/javascript"></script>
+<script src="{{asset('/assets/bootstrap-fileinput/js/plugins/sortable.js')}}" type="text/javascript"></script>
+<script src="{{asset('/assets/bootstrap-fileinput/js/fileinput.js')}}" type="text/javascript"></script>
+<script src="{{asset('/assets/bootstrap-fileinput/themes/fas/theme.js')}}" type="text/javascript"></script>
+<script src="{{asset('/assets/bootstrap-fileinput/themes/explorer-fas/theme.js')}}"
+        type="text/javascript"></script>
 <script>
     $(document).ready(function () {
         $(".kv-explorer").fileinput({
@@ -43,9 +30,9 @@
             initialPreviewAsData: true,
             showUpload: false,
             showRemove: true,
-            dropZoneEnabled: true,
+            dropZoneEnabled: false,
             maxFileSize: 512000000,
-            maxFileCount: 30,
+            maxFileCount: 10,
             uploadUrl: '#',
         });
     });
